2152eacfb69bbf90ae4553a129c18789478f1c5d
Jose Ulloa <jandres258@hotmail.com>
2023-03-13 19:37:20 -0600

    Basic mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- windows
+vim.keymap.set("n", "<leader>Ww", "<C-W>p", { desc = "Other window" })
+vim.keymap.set("n", "<leader>Wd", "<C-W>c", { desc = "Delete window" })
+vim.keymap.set("n", "<leader>W-", "<C-W>s", { desc = "Split window below" })
+vim.keymap.set("n", "<leader>W|", "<C-W>v", { desc = "Split window right" })
+
+-- file
+vim.keymap.set({"n", "i"}, "<C-z>", "<cmd>u<cr>", { desc = "Undo" })
+vim.keymap.set({"n", "i"}, "<C-s>", "<cmd>w!<cr>", { desc = "Force write" })
+vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit" })
+vim.keymap.set("n", "<leader>w", "<cmd>w!<cr>", { desc = "Write" })
+
+-- clipboard
+vim.keymap.set("x", "p", [["_dP]], { desc = "Paste and keep"})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Not sure" })
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], {})
+
+-- others
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "kk", "<Esc>")
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,35 @@
+local Util = require("lazyvim.util")
+
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      local keymaps = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["gz"] = { name = "+surround" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader><tab>"] = { name = "+tabs" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>c"] = { name = "+code" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>S"] = { name = "+session" },
+        ["<leader>q"] = { name = "+quit/search" },
+        ["<leader>u"] = { name = "+ui" },
+        ["<leader>W"] = { name = "+windows" },
+        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      }
+      if Util.has("noice.nvim") then
+        keymaps["<leader>sn"] = { name = "+noice" }
+      end
+      wk.register(keymaps)
+    end,
+  }
